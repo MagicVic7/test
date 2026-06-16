5fafa799b518c76abf4fb873ce45311bb905cfb6
MagicVic7 <victor2007.dalle@gmail.com>
2026-06-16 23:00:21 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'magic-chrono-brainstorm-v7-stable';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))); self.clients.claim(); });
-self.addEventListener('fetch', event => { const url = new URL(event.request.url); if (event.request.method !== 'GET') return; if (url.hostname !== self.location.hostname) return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
