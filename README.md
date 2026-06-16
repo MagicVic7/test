7794afe2645cb2dda24e18be6947c62522bdd95e
MagicVic7 <victor2007.dalle@gmail.com>
2026-06-16 18:59:51 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# test
+# Magic Chrono Brain Storm V5.2
+
+Parser API Brain Storm renforcé : accepte JSON ou texte brut, cherche position, annif, birthday, naissance, ou nombres dans la réponse.
---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'magic-chrono-brainstorm-v52';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))); self.clients.claim(); });
+self.addEventListener('fetch', event => { const url = new URL(event.request.url); if (event.request.method !== 'GET') return; if (url.hostname !== self.location.hostname) return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
